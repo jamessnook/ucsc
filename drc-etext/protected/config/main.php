b8fe714a050d0ae4c0de869fb1dba631b109c173
Jim Snook <jsnook@ucsc.edu>
2013-01-22 00:01:29 -0800

    update

--- drc-etext/protected/config/main.php
+++ drc-etext/protected/config/main.php
@@ -69,20 +69,20 @@ return array(
 				//'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
+		/*
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/etext.db',
 		),
+		*/
 		// uncomment the following to use a MySQL database
-		/*
 		'db'=>array(
- 			'class'=>'ext.base.CloudDbConnection',			
- 			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+ 			//'class'=>'ext.base.CloudDbConnection',			
+ 			'connectionString' => 'mysql:host=localhost;dbname=etext',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
 			'charset' => 'utf8',
 		),
-		*/
 		'authManager'=>array(
             'class'=>'CDbAuthManager',
             'connectionID'=>'db',
